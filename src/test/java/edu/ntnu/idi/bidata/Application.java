897de6734efe1dd43c667cf9fe27af1b703106d3
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2024-11-11 17:26:50 +0100

    0.7.0 Opprettet application.java for å behandle applikasjonen og er en instans av UserUI.java.

---
+++ src/test/java/edu/ntnu/idi/bidata/Application.java
@@ -0,0 +1,10 @@
+package edu.ntnu.idi.bidata;
+
+public class Application {
+
+  public static void main(String[] args) {
+    UserUI userUI = new UserUI();
+    userUI.init();
+    userUI.start();
+  }
+}
--- src/test/java/edu/ntnu/idi/bidata/UserUI.java
+++ src/test/java/edu/ntnu/idi/bidata/UserUI.java
@@ -1,15 +1,16 @@
 package edu.ntnu.idi.bidata;
 
-import java.sql.SQLOutput;
+//UserUI klasse for å teste GroceryKlassen.
+
 import java.time.LocalDate;
 
 public class UserUI {
 
-  public void init() {
+  public void init() {  //Metode for å teste initialisering av systemet.
     System.out.println("Initializing system");
   }
 
-  public void start() {
+  public void start() { //Metode for å teste at Groceryklassen fungerer.
     System.out.println("Testing class 'Grocery'");
 
     Grocery milk = new Grocery("Milk", 2, "Litre", 20, LocalDate.of(2024, 11, 20));
