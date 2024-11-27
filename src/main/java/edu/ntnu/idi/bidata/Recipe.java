c5b4e97439190a43c7985001edca12e570d0a220
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2024-11-27 22:46:39 +0100

    1.7.5 Added a method for adding a grocery as an ingredient to the recipe. Added getters. Added a to-string method (not final).

--- src/main/java/edu/ntnu/idi/bidata/Recipe.java
+++ src/main/java/edu/ntnu/idi/bidata/Recipe.java
@@ -17,6 +17,10 @@ public class Recipe {
   //private final String nutrients; Add macro nutrients into own class in the future.
 
   public Recipe(String name, String description, String procedure, int time, int servings) {
+    if (servings <= 0) {
+      throw new IllegalArgumentException("Servings must be greater than 0");
+    }
+
     this.name = name;
     this.description = description;
     this.procedure = procedure;
@@ -24,5 +28,51 @@ public class Recipe {
     this.time = time;
     this.servings = servings;
     //this.nutrients = nutrients; Add macro nutrients into own class in the future.
+  }
+
+  /**
+   * Adds a grocery as an ingredient to the recipe.
+   *
+   * @param grocery The grocery to add.
+   * @throws IllegalArgumentException If the grocery is null.
+   */
+  public void addIngredient(Grocery grocery) {
+    if (grocery == null) {
+      throw new IllegalArgumentException("Grocery cannot be null");
+    }
+    ingredients.put(grocery.getName(), grocery);
+  }
+
+  /**
+   * Get-methods to get the values of the recipes.
+   */
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getProcedure() {
+    return procedure;
+  }
+
+  public Map<String, Grocery> getIngredients() {
+    return ingredients;
+  }
+
+  public int getTime() {
+    return time;
+  }
+
+  public int getServings() {
+    return servings;
+  }
+
+  @Override
+  public String toString() {
+    return "Recipe [name=" + name + ", description=" + description + ", procedure=" + procedure
+        + ", ingredients=" + ingredients + ", time=" + time + ", servings=" + servings + "]";
   }
 }
