301081ee48a4911c8179dc86bb7c82bba48c50fb
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2024-12-02 13:15:16 +0100

    2.0 Updated class "Application" and class "UserUI" to test some of the functionalities of the classes.

--- src/main/java/edu/ntnu/idi/bidata/UserUI.java
+++ src/main/java/edu/ntnu/idi/bidata/UserUI.java
@@ -1,6 +1,7 @@
 package edu.ntnu.idi.bidata;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Class "UserUI" has the responsibility of testing the "Grocery" class and handle user interface.
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 public class UserUI {
 
   private Fridge fridge;
+  private CookBook cookBook;
 
   /**
    * Initializes the system while printing "Initializing system" to indicate that the system is
@@ -15,6 +17,7 @@ public class UserUI {
    */
   public void init() {
     fridge = new Fridge();
+    CookBook cookBook = new CookBook();
     System.out.println("Initializing system");
   }
 
@@ -23,25 +26,29 @@ public class UserUI {
    * the functionality of the "Fridge" class.
    */
   public void start() {
-    fridge.registerGrocery(new Grocery("Milk", 1, "l", 20, LocalDate.of(2024, 11, 20)));
-    fridge.registerGrocery(new Grocery("Butter", 500, "g", 50, LocalDate.of(2024, 11, 30)));
-    fridge.registerGrocery(new Grocery("Eggs", 6, "pcs", 40, LocalDate.of(2024, 11, 22)));
-    fridge.registerGrocery(new Grocery("Bacon", 5, "pcs", 80, LocalDate.of(2024, 11, 25)));
+    fridge.registerGrocery(new Grocery("Milk", 500, "ml", 20, LocalDate.of(2024, 12, 20)));
+    fridge.registerGrocery(new Grocery("Flour", 300, "g", 5, LocalDate.of(2024, 12, 23)));
+    fridge.registerGrocery(new Grocery("Butter", 500, "g", 50, LocalDate.of(2024, 12, 30)));
+    fridge.registerGrocery(new Grocery("Eggs", 6, "pcs", 40, LocalDate.of(2024, 12, 22)));
+    fridge.registerGrocery(new Grocery("Bacon", 5, "pcs", 80, LocalDate.of(2024, 12, 25)));
 
     System.out.println(fridge);
 
-    System.out.println("\nRemoving 1 liter of milk:");
-    fridge.removeGrocery("Milk", 1);
-    System.out.println(fridge);
+    Recipe pancakes = new Recipe("Pancakes", "Fluffy pancakes", "Mix and fry", 10, 4);
+    pancakes.addIngredient(new Grocery("Milk", 300, "ml", 20, LocalDate.of(2024, 12, 20)));
+    pancakes.addIngredient(new Grocery("Flour", 200, "g", 5, LocalDate.of(2024, 12, 23)));
 
-    System.out.println("\nSearching for butter:");
-    Grocery butter = fridge.searchGrocery("Butter");
-    System.out.println(butter);
+    Recipe waffles = new Recipe("Waffles", "Crispy waffles", "Mix and cook", 15, 4);
+    waffles.addIngredient(new Grocery("Milk", 200, "ml", 20, LocalDate.of(2024, 12, 20)));
+    waffles.addIngredient(new Grocery("Flour", 100, "g", 5, LocalDate.of(2024, 12, 23)));
 
-    System.out.println("\nExpired groceries as of 2024-11-20:");
-    fridge.getExpiredGroceries(LocalDate.of(2024, 11, 25)).forEach(System.out::println);
+    cookBook.addRecipe(pancakes);
+    cookBook.addRecipe(waffles);
 
-    System.out.println("\nSorted list of groceries:");
-    fridge.getSortedGroceries().forEach(System.out::println);
+    List<Recipe> possibleRecipes = cookBook.findAllPossibleRecipes(fridge);
+    System.out.println("Recipes you can make:");
+    for (Recipe recipe : possibleRecipes) {
+      System.out.println(recipe.getName());
+    }
   }
 }
